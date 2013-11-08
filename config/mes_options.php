c0e34fc921ff51a51196642c0bb50179d270c3ba
Michel Bystranowski <michel@vertige.org>
2013-11-08 10:46:55 +0100

    importe le mes_options.php qu'il y a sur le serveur de prod

--- config/mes_options.php
+++ config/mes_options.php
@@ -1,13 +1,2 @@
 <?php
-define ('_ID_WEBMESTRES', '1514');
-
-$GLOBALS['id_mentions_legales_fr'] = 574;
-$GLOBALS['id_mentions_legales_nl'] = 575;
-$GLOBALS['id_mentions_legales_en'] = 576;
-
-function autoriser_travaux($faire, $quoi, $id, $qui, $opts) {
-  if ($qui['statut']=='0minirezo' OR $qui['statut']=='1comite')
-    return true;
-  return false;
-}
-?>
+define ('_ID_WEBMESTRES', '1:4:1514');
