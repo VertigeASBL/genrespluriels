83d28f4dffdd2a409b28a804f46bf47328956837
damazone <londe.ngosso@gmail.com>
2013-11-09 00:58:50 +0100

    Attention !

--- config/_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
-$GLOBALS['spip_connect_version'] = 0.4;
-spip_connect_db('mysql.domainepublic.net','','genrespluriels','lomaxtan16102007','genrespluriels');
-?>
